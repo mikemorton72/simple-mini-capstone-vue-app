b9f742b4db58897699fcf5058ae83a9cd3538bfc
Mike Morton <mmorton7295@gmail.com>
2021-07-18 10:19:19 -0500

    adds productsIndex method that pulls from simple-mini-capstone-api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,32 @@
 <template>
   <div class="home">
     <h1>Simple Products App</h1>
-    <hr>
+    <hr />
     <div v-for="product in products" v-bind:key="product.id">
       <p>Product name: {{ product.name }}</p>
       <p>Product price: {{ product.price }}</p>
-      <hr>
+      <hr />
     </div>
   </div>
 </template>
 <style></style>
 <script>
+import axios from "axios";
 export default {
   data: function () {
     return {
-      products: [
-        { id: 1, name: "Product 1", price: 10 },
-        { id: 2, name: "Product 2", price: 230 },
-      ],
+      products: [],
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    this.productsIndex();
+  },
+  methods: {
+    productsIndex: function () {
+      axios.get("http://localhost:3000/products").then((response) => {
+        this.products = response.data;
+      });
+    },
+  },
 };
 </script>
