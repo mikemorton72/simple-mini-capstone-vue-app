d5bbbc033c201e1a8900bfafd87a503e490b4480
Mike Morton <mmorton7295@gmail.com>
2021-07-18 11:27:30 -0500

    adds create function to products

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,33 @@
 <template>
   <div class="home">
     <h1>Simple Products App</h1>
+    <p>Name: <input type="text" v-model="newProduct.name" /></p>
+    <p>Description: <input type="text" v-model="newProduct.description" /></p>
+    <p>Price: <input type="text" v-model="newProduct.price" /></p>
+    <p>image_url: <input type="text" v-model="newProduct.image_url" /></p>
+    <button v-on:click="productsCreate">Add Product</button>
     <hr />
-    <div v-for="product in products" v-bind:key="product.id">
-      <p>Product name: {{ product.name }}</p>
-      <p>Product price: {{ product.price }}</p>
+    <div v-for="product in products" :key="product.id">
+      <p>Product: {{ product.name }}</p>
+      <img v-bind:src="product.image_url" />
+      <p>Description: {{ product.description }}</p>
+      <p>Price: {{ product.price }}</p>
       <hr />
     </div>
   </div>
 </template>
-<style></style>
+<style>
+img {
+  height: 250px;
+}
+</style>
 <script>
 import axios from "axios";
 export default {
   data: function () {
     return {
       products: [],
+      newProduct: {},
     };
   },
   created: function () {
@@ -26,6 +38,14 @@ export default {
       axios.get("http://localhost:3000/products").then((response) => {
         this.products = response.data;
       });
+    },
+    productsCreate: function () {
+      axios
+        .post("http://localhost:3000/products", this.newProduct)
+        .then((response) => {
+          this.products.push(response.data);
+          this.newProduct = {};
+        });
     },
   },
 };
