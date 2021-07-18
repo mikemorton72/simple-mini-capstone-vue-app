457629632b9f6ffac044d20b84f0c2e756cdfc86
Mike Morton <mmorton7295@gmail.com>
2021-07-18 13:05:11 -0500

    adds a show function for each product using a dialogue box

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,8 +14,18 @@
       <img v-bind:src="product.image_url" />
       <p>Description: {{ product.description }}</p>
       <p>Price: {{ product.price }}</p>
+      <button v-on:click="productsShow(product)">Show Info</button>
       <hr />
     </div>
+    <dialog id="product-details">
+      <form method="dialog">
+        <h2>{{ currentProduct.name }}</h2>
+        <img v-bind:src="currentProduct.image_url" />
+        <p>{{ currentProduct.description }}</p>
+        <p>Price: {{ currentProduct.price }}</p>
+        <button>Close</button>
+      </form>
+    </dialog>
   </div>
 </template>
 <style>
@@ -30,6 +40,8 @@ export default {
     return {
       products: [],
       newProduct: {},
+      currentProduct: {},
+      errors: {},
     };
   },
   created: function () {
@@ -47,7 +59,16 @@ export default {
         .then((response) => {
           this.products.push(response.data);
           this.newProduct = {};
+        })
+        .catch((error) => {
+          console.log(error.response.data.errors);
+          this.errors = error.response.data.errors;
         });
+    },
+    productsShow: function (product) {
+      this.currentProduct = product;
+      console.log(this.currentProduct);
+      document.querySelector("#product-details").showModal();
     },
   },
   computed: {
